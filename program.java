3266d623397124fd7409381d8d102b08464fca99
Ankit Pandey <ankitapandey1576@gmail.com>
2023-10-26 17:02:32 +0530

    commit done

---
+++ program.java
@@ -0,0 +1,11 @@
+Class progra{
+    public static void main(String []args)
+    {
+        int a=;
+        int b=
+        intc;
+        c=a+b;
+        System.out.println("sum= "+c);
+    }
+    
+}
